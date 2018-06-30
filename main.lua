08b26bbc676462d2431107ad0cfa879ce4309e3b
LUCIANO CAMPOS DE LIMA JUNIOR <20161D13GR0210@LABS.GARANHUNS.IFPE>
2018-06-29 21:54:16 -0300

    Luciano quebrou o codigo dinovo

--- main.lua
+++ main.lua
@@ -9,9 +9,7 @@ vidas = 5
 scoreAtual = 0
 hiScore = 0
 
--- =========================================================================================== 
 
--- =========================================================================================== --
 
 function gerarFuel( )
 	addFuel = display.newImage("addFuel.png", math.random(70,250), -300)
@@ -20,67 +18,22 @@ function gerarFuel( )
 	addFuel:setLinearVelocity(0,50)
 end
 
-gerarGasolina = timer.performWithDelay(5000, gerarFuel,0)
+geraFuel = timer.performWithDelay(5000, gerarFuel,0)
 
 function criarCenario(event)
 	
 	 if (event.phase == "began") then
 			limparAbrirJogo()  
-			cenarioCentral = display.newRect(display.contentWidth*0.5, 100,200,600)
-			cenarioCentral.strokeWidth = 0
-			cenarioCentral:setFillColor( 0,0,1 )
-
 			
-
-			cenarioDireito = display.newImage("lateral1.jpg",290,100)
-			cenarioEsquerdo = display.newImage("lateral1.jpg", 30,100)
-			
-
-			cenarioPedra1 = display.newImage("pedra1.png",( math.random (-20, 80)) , ( math.random (-100, 0)))
-			cenarioPedra2 = display.newImage("pedra2.png",( math.random (290, 300)) , ( math.random (-500, -100)))
-			cenarioPedra3 = display.newImage("pedra3.png",( math.random (-10, 50)) , ( math.random (-600, -200)))
-
-
-			cenarioRodape = display.newRect(display.contentWidth*0.5,490, display.contentWidth, 100)
-			cenarioRodape.strokeWidth = 1
-			cenarioRodape:setFillColor( 0,0,0 )
-
-
-			botaoEsquerda = display.newImage ("botaoEsquerda.png", 50, 480 )
-			botaoDireita = display.newImage("botaoDireita.png",270, 480 )			
-			botaoTiro = display.newImage("botaoTiro3.png", display.contentWidth*0.5, 480 )
-
-			cenarioScore = display.newRect(display.contentWidth/2, 420, 350, 40 )
-			cenarioScore:setFillColor( 0,0,0 )
-			cenarioLife = display.newImage("life.png", display.contentWidth - 40, 410 )
-			cenariolioApagaLife = display.newRect(356,410,80,15)
-			cenariolioApagaLife:setFillColor(0,0,0)
-			cenarioCombustivel = display.newImage("fuel.png", display.contentWidth - 35, 428)
-			cenarioApagaCombustivel = display.newRect(360,428,80,8)
-			cenarioApagaCombustivel:setFillColor( 01,0.5,0.5 )
-
-			nave = display.newImage("nave1.png", display.contentWidth/2, 380)
-			physics.addBody(nave, "static")
-			nave:addEventListener("collision", decrementarVida)
-
-			
-			botaoEsquerda:addEventListener("touch", moverNaveEsquerda)
-			botaoDireita:addEventListener("touch", moverNaveDireita)
-			botaoTiro:addEventListener("touch", atirar)		
-			
-			
-			cenarioBarraScore = display.newImage("barraScore.png", display.contentWidth/2+10 	, 424)
-			cenarioScore = display.newImage("score.png", 62,423)
-
-	-------------------------------------------------------------------------------
-			descerCenario = timer.performWithDelay(10, movimentarCenario,0) 
 			--movimentoInimigos = timer.performWithDelay(10, movimentarinimigo,0)
 			--incrementoScore = timer.performWithDelay(1000,incrementarScore,0)
 	-------------------------------------------------------------------------------
 
-	criarInimigo()
+	--criarInimigo()
 	end
 end
+
+
 
 function criarInimigo()
 --	inimigo4 = display.newImage("inimigo4.png", (math.random (70, 250)),(math.random (-900, -60)))
@@ -98,7 +51,9 @@ function criarInimigo()
 
 end
 
-gerarInimigo = timer.performWithDelay(3000, criarInimigo,0)
+--criarInimigo()
+
+gerarInimigo = timer.performWithDelay(1000, criarInimigo,0)
 -- =========================================================================================== --
 
 local pontuacaoText = display.newText("0",0,0)
@@ -155,23 +110,11 @@ function zerouJogo()
 end
 -- ============================================================= --
 
-function limparAbrirJogo()
-
-		botaoSim:removeSelf()
-		botaoNao:removeSelf()
-		--fundo:removeSelf()
-		inicio:removeSelf()
-	
-end
 
 
 -- =============================================================================================== --
 
-function gameOver()
-	if scoreAtual > hiScore then
-		hiScore = scoreAtual
-	end
-end
+
 -- =============================================================================================== --
 
 function movimentarCenario()
@@ -181,7 +124,6 @@ function movimentarCenario()
 	
 		cenarioEsquerdo.y = cenarioEsquerdo.y + velocidadeCenario
 		cenarioDireito.y = cenarioDireito.y + velocidadeCenario
-		--addFuel.y= addFuel.y +velocidadeCenario
 		cenarioPedra1.y = cenarioPedra1.y + velocidadeCenario
 		cenarioPedra2.y = cenarioPedra2.y + velocidadeCenario
 		cenarioPedra3.y = cenarioPedra3.y + velocidadeCenario
@@ -199,9 +141,9 @@ function movimentarCenario()
 		end
 	
 		if(vidas >=1)then
-			cenarioApagaCombustivel.x = cenarioApagaCombustivel.x - 0.05
+			cenarioApagaCombustivel.x = cenarioApagaCombustivel.x - 0.5
 		else
-		abrirJogo()
+		--abrirJogo()
 		end
 
 
@@ -292,12 +234,12 @@ function adicionarVida(event)
 		
 		display.remove(event.target)
 		addFuel = nil
-		if event.phase == "began" then
+		
 			if vidas < 5 then
-			cenariolioApagaLife.x = cenariolioApagaLife.x + 15
+			--cenariolioApagaLife.x = cenariolioApagaLife.x + 15
 			vidas = vidas+1 
 			end
-		end
+		
 		
 		print("Quantidade de vida: " .. vidas)
 
@@ -309,14 +251,12 @@ end
 function decrementarVida(event)
 		
 		display.remove(event.other)
-		--inimigo1 = nil
-		addFuel = nil
-		if event.phase == "began" then
+		inimigo1 = nil
 			if vidas > 0 then
-				cenariolioApagaLife.x = cenariolioApagaLife.x - 15
+				--cenariolioApagaLife.x = cenariolioApagaLife.x - 15
 				vidas = vidas - 1
 			end
-		end
+		
 	print("Quantidade de vidas: ".. vidas)
 	
 end
@@ -371,17 +311,69 @@ function matarInimigo(event)
 end
 
 
-function abrirJogo()
+--function abrirJogo()
 
-	inicio = display.newImage("inicio.png", display.contentWidth /2+4, 160)
+	--inicio = display.newImage("inicio.png", display.contentWidth /2+4, 160)
 	
 	botaoSim = display.newImage("botaoSim.png",100 ,220)
 	botaoSim:addEventListener("touch", criarCenario)
 	
 	botaoNao = display.newImage("botaoNao.png", 230,220)
-	botaoNao:addEventListener("touch", limparAbrirJogo)
+	--botaoNao:addEventListener("touch", limparAbrirJogo)
 
-end
+--end
+cenarioCentral = display.newRect(display.contentWidth*0.5, 100,200,600)
+cenarioCentral.strokeWidth = 0
+cenarioCentral:setFillColor( 0,0,1 )
+
+
+
+cenarioDireito = display.newImage("lateral1.jpg",290,100)
+cenarioEsquerdo = display.newImage("lateral1.jpg", 30,100)
+
+
+cenarioPedra1 = display.newImage("pedra1.png",( math.random (-20, 80)) , ( math.random (-100, 0)))
+cenarioPedra2 = display.newImage("pedra2.png",( math.random (290, 300)) , ( math.random (-500, -100)))
+cenarioPedra3 = display.newImage("pedra3.png",( math.random (-10, 50)) , ( math.random (-600, -200)))
+
+
+cenarioRodape = display.newRect(display.contentWidth*0.5,490, display.contentWidth, 100)
+cenarioRodape.strokeWidth = 1
+cenarioRodape:setFillColor( 0,0,0 )
+
+
+
+
+cenarioScore = display.newRect(display.contentWidth/2, 420, 350, 40 )
+cenarioScore:setFillColor( 0,0,0 )
+cenarioLife = display.newImage("life.png", display.contentWidth - 40, 410 )
+--cenariolioApagaLife = display.newRect(356,410,80,15)
+--cenariolioApagaLife:setFillColor(0,0,0)
+cenarioCombustivel = display.newImage("fuel.png", display.contentWidth - 35, 428)
+cenarioApagaCombustivel = display.newRect(360,428,80,8)
+cenarioApagaCombustivel:setFillColor( 01,0.5,0.5 )
+
+
+cenarioBarraScore = display.newImage("barraScore.png", display.contentWidth/2+10 	, 424)
+cenarioScore = display.newImage("score.png", 62,423)
+
+-------------------------------------------------------------------------------
+--descerCenario = timer.performWithDelay(10, movimentarCenario,0) 
+
 
 --
-abrirJogo()
+
+botaoEsquerda = display.newImage ("botaoEsquerda.png", 50, 480 )
+botaoDireita = display.newImage("botaoDireita.png",270, 480 )			
+botaoTiro = display.newImage("botaoTiro3.png", display.contentWidth*0.5, 480 )
+
+
+nave = display.newImage("nave1.png", display.contentWidth/2, 380)
+physics.addBody(nave, "static")
+nave:addEventListener("collision", decrementarVida)
+
+botaoEsquerda:addEventListener("touch", moverNaveEsquerda)
+botaoDireita:addEventListener("touch", moverNaveDireita)
+botaoTiro:addEventListener("touch", atirar)	
+
+--abrirJogo()
